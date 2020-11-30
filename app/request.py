08c20da02223e7aaa2c1b1eb20b8b0ca18aea94c
nadineuwineza <nadineuwineza2017@gmail.com>
2020-11-30 12:08:41 +0200

    added method sources categories

--- app/request.py
+++ app/request.py
@@ -11,3 +11,32 @@ def configure_request(app):
     api_key = app.config['NEWS_API_KEY']
     base_url = app.config['NEWS_SOURCE_URL']
     article_url = app.config["ARTICLES_URL"]
+
+def get_sources(category):
+    get_sources_url = base_url.format(category, api_key)
+
+    with urllib.request.urlopen(get_sources_url) as url:
+        get_sources_data = url.read()
+        get_source_response = json.loads(get_sources_data)
+
+        sources_list = None
+        if get_source_response['sources']:
+            rest_here = get_source_response['sources']
+            sources_list = process_results(rest_here)
+
+    return sources_list
+
+def process_results(results):
+    sources_list = []
+
+    for item in results:
+        id = item.get('id')
+        description = item.get('description')
+        url = item.get('url')
+        category = item.get('category')
+
+        if description:
+            new_item = Source(id, description, url, category)
+            sources_list.append(new_item)
+        
+    return sources_list
