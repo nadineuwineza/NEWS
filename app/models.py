38abe4e829ad5082db6015231746a6ebef714723
nadineuwineza <nadineuwineza2017@gmail.com>
2020-11-30 13:02:49 +0200

    added models

---
+++ app/models.py
@@ -0,0 +1,18 @@
+class Source:
+
+    source_list = []
+
+    def __init__(self, id, description, url, category):
+        self.id = id
+        self.description = description
+        self.url = url
+        self.category = category
+
+class Articles:
+    
+    def __init__(self, id,author, title, urlToImage, url):
+        self.id = id
+        self.author = author
+        self.title = title
+        self.urlToImage= urlToImage
+        self.url = url
